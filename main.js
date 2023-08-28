cf1762c3cb4655b634a98279d507ce8a26ba1284
Akbar <sulajmankulovakbar@gmail.com>
2023-08-28 16:47:29 +0300

    Akbar HW3_2

---
+++ main.js
@@ -0,0 +1,25 @@
+//1 задание
+// let a = [1, 3, 5, 8];
+// let b = [1, 3, 5, 8];
+
+// function equal(x, y) {
+// return x.length === y.length &&
+//         x.every((element, index) => y[index] === element);
+// }
+
+// console.log(equal(a, b));
+
+// 2 задание
+// function pow(number , degree){
+//     return  Math.pow(number , degree)
+// }
+// const result = pow(2,3);
+// console.log(result);
+
+
+// 1 доп
+// const sms = (phone,text) => {
+//     alert(phone, text)
+// }
+
+// sms(+996500200, "Naela");
